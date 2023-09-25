390cad36abc41ed0316f0c6a5bcea5d682330478
ArtemPchela <pchelenkovartem@gmail.com>
2023-09-25 13:19:21 +0200

    final version

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import typescript from "rollup-plugin-typescript2";
+import external from "rollup-plugin-peer-deps-external";
+import postcss from 'rollup-plugin-postcss';
+
+import pkg from "./package.json";
+
+export default {
+  input: "src/index.tsx",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      exports: "named",
+      sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: "es",
+      exports: "named",
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    external(),
+    typescript(),
+    postcss(),
+  ]
+};
---
+++ src/index.tsx
@@ -0,0 +1,67 @@
+"use client"
+import React, { useEffect, useState } from "react";
+import "./index.css";
+
+type ScrollToTopProps = {
+    displayType?: "text" | "htmlArrow" | "image";
+    imageSrc?: string;
+    myClass?: string;
+    text?: string;
+    threshold?: number;
+};
+
+const scrollToTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+};
+
+const ScrollToTopOptions = ({
+    displayType = "text",
+    imageSrc,
+    myClass = "",
+    text = "Top",
+    threshold = 150,
+    }: ScrollToTopProps) => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsVisible(window.scrollY > threshold);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, [threshold]);
+
+    const commonProps = {
+        className: isVisible
+            ? `scroll_top ${myClass} visible `
+            : `scroll_top ${myClass}`,
+        onClick: scrollToTop,
+    };
+
+    let content;
+
+    switch (displayType) {
+        case "text":
+            content = <div {...commonProps}>{text}</div>;
+            break;
+        case "htmlArrow":
+            content = <div {...commonProps}>&#8682;</div>;
+            break;
+        case "image":
+            content = imageSrc ? (
+                <img src={imageSrc} alt="To top" {...commonProps} />
+            ) : (
+                <div {...commonProps}>{text}</div>
+            );
+            break;
+    }
+
+    return content;
+};
+
+export default ScrollToTopOptions;
